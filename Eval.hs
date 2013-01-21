b4ed016123762e0222938303d64393bd2141dfa0
IngCr3at1on <sblood86@gmail.com>
2013-01-20 20:02:05 -0600

    Add filter and dev notes.

--- Eval.hs
+++ Eval.hs
@@ -12,7 +12,8 @@ import Write
 -- SndNick -> SndFrom (channel/privchat etc) -> content (command)
 eval :: String -> String -> String -> Net ()
 -- Non-argumental commands (keep in alpha)
-eval _ _ "!quit" = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
+-- I'm unable to make this apply to a list of users instead of the single option
+eval "IngCr3at1on" _ "!quit" = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
 -- Single arg commands
 eval "IngCr3at1on" _ x | "!id " `isPrefixOf` x = privmsg (drop 4 x) 
 eval "IngCr3at1on" _ x | "!join " `isPrefixOf` x = write "JOIN" (drop 6 x)
