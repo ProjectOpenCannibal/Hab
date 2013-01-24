8906454575fb8c432148557d4e19b5d6687914e1
IngCr3at1on <sblood86@gmail.com>
2013-01-24 01:30:56 -0600

    Clean up evaluation code a little bit.

--- Eval.hs
+++ Eval.hs
@@ -14,9 +14,12 @@ eval :: String -> String -> String -> Net ()
 -- Non-argumental commands (keep in alpha)
 -- I'm unable to make this apply to a list of users instead of the single option
 eval "IngCr3at1on" _ "!quit" = write "QUIT" ":Reloading, hopefully..." >> io (exitWith ExitSuccess)
+
 -- Single arg commands
-eval "IngCr3at1on" _ x | "!id " `isPrefixOf` x = privmsg (drop 4 x)
-eval "IngCr3at1on" _ x | "!join " `isPrefixOf` x = write "JOIN" (drop 6 x)
-eval "IngCr3at1on" _ x | "!kick " `isPrefixOf` x = write "KICK" (drop 6 x)
-eval "IngCr3at1on" _ x | "!part " `isPrefixOf` x = write "PART" (drop 6 x)
+eval "IngCr3at1on" _ x
+    | "!id " `isPrefixOf` x = privmsg (drop 4 x)
+    | "!join " `isPrefixOf` x = write "JOIN" (drop 6 x)
+    | "!kick " `isPrefixOf` x = write "KICK" (drop 6 x)
+    | "!part " `isPrefixOf` x = write "PART" (drop 6 x)
+
 eval _ _ _ = return () -- ignore everything else
