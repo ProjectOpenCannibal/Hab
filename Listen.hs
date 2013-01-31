4484310867e44d034f0248f14f220215f7e4aaf4
IngCr3at1on <sblood86@gmail.com>
2013-01-31 13:13:57 -0600

    Style fixes and dev notes: * remove whitespace and fix if structure per Haskell standards. * added an untested, unfinished modechange check to Listen.hs (this is for FMKilo, wanted a way to parse mode changes, I forget why)

--- Listen.hs
+++ Listen.hs
@@ -15,14 +15,21 @@ listen :: Handle -> Net ()
 listen h = forever $ do
     s <- init `fmap` io (hGetLine h)
     io (putStrLn s)
-    if ping s then pong s else eval (sndnick s) (sndfrom s) (content s)
+    if ping s 
+        then pong s
+        --else if modechange s
+            --then evalmode s
+        else eval (sndnick s) (sndfrom s) (content s)
   where
     forever a = a >> forever a
     sndnick = drop 1 . takeWhile (/= '!')
-    -- I don't think this is parsing properly; if I set it to CannibalismBot
-    -- it should only respond to private message but instead it does not respond
-    -- at all.
+    -- I don't think this is parsing properly; if I set it to Hab it should only
+    -- respond to private message but instead it does not respond at all.
     sndfrom = dropWhile (/= ' ') . dropWhile (/= ' ') . takeWhile (/= ' ')
     content = drop 1 . dropWhile (/= ':') . drop 1
-    ping x	= "PING :" `isPrefixOf` x
-    pong x	= write "PONG" (':' : drop 6 x)
+    ping x = "PING :" `isPrefixOf` x
+    pong x = write "PONG" (':' : drop 6 x)
+    -- Untested method of passing a modechange (example for FMKilo) still needs
+    -- an evalmode function which I would recommend adding into Eval.hs and
+    -- exporting it along with eval.
+    --modechange x = dropWhile (/= '#') x
