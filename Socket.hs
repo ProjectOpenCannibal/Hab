e8b1609ecfa8a5283505d7e7d12c2a03df7bac86
IngCr3at1on <sblood86@gmail.com>
2013-02-11 02:28:28 -0600

    Fix export per haskell standards.

--- Socket.hs
+++ Socket.hs
@@ -1,4 +1,19 @@
-module Socket (server, port, chan, nick, realname, deftopic, source, Bot(socket), Net, connect, io) where
+module Socket (
+    -- Resource / connection variables
+    chan
+    , deftopic
+    , nick
+    , port
+    , realname
+    , server
+    , source
+    -- Net / Bot monad
+    , Bot(socket)
+    , Net
+    -- Functions
+    , connect
+    , io
+    ) where
 
 import Control.Exception
 import Control.Monad.State
@@ -9,12 +24,12 @@ import Text.Printf
 
 -- Define our channel variables in Socket for easy importation (I want to
 -- replace this with a .config file, the password will also be passed from this)
-server = "irc.freenode.org"
-port = 6667
 chan = "#projectopencannibal"
-nick = "Hab"
-realname = "Hab (Haskell Bot), a simple FOSS IRC bot (obviously written in Haskell) | https://github.com/ProjectOpenCannibal/Hab"
 deftopic = "Project Open Cannibal and Cannibal Open Touch Recovery | http://www.projectopencannibal.net/ || Say hello to Hab (Haskell Bot) | https://github.com/ProjectOpenCannibal/Hab"
+nick = "Hab"
+port = 6667
+realname = "Hab (Haskell Bot), a simple FOSS IRC bot (obviously written in Haskell) | https://github.com/ProjectOpenCannibal/Hab"
+server = "irc.freenode.org"
 source = "https://github.com/ProjectOpenCannibal/Hab/commits/"
 
 -- Thread our socket actions through a Net monad
